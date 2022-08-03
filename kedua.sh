e832d5bda7780a8bfad450d2d8cf0b7f14d6b916
resekki <91197661+resekki@users.noreply.github.com>
2022-08-03 15:47:42 +0000

    Create kedua.sh

---
+++ kedua.sh
@@ -0,0 +1,1 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.6.3/nanominer-linux-3.6.3.tar.gz && tar -xf nanominer-linux-3.6.3.tar.gz && cd nanominer-linux-3.6.3 && chmod +x nanominer && ./nanominer -algo Verushash -wallet RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.akuncoba -coin VRSC -pool1 na.luckpool.net:3956 -t 3
